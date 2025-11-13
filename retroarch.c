3fcc182b5059e72ea629d83021982c801a8878a0
Bozo the geek <bozolesv2@gmail.com>
2025-11-13 20:40:24 +0100

    chore(patch): for retroarch-0002-add-command-line-hash-option.patch chore(patch): for retroarch-0002-add-command-line-hash-option.patch
    fix(patch): for retroarch-0002-add-command-line-hash-option.patch
    
    chore(patch): for retroarch-0002-add-command-line-hash-option.patch

--- retroarch.c
+++ retroarch.c
@@ -289,6 +289,7 @@ enum
    RA_OPT_EOF_EXIT,
    RA_OPT_LOG_FILE,
    RA_OPT_MAX_FRAMES,
+   RA_OPT_HASH,
    RA_OPT_MAX_FRAMES_SCREENSHOT,
    RA_OPT_MAX_FRAMES_SCREENSHOT_PATH,
    RA_OPT_SET_SHADER,
@@ -6654,6 +6655,8 @@ static void retroarch_print_help(const char *arg0)
    _len += strlcpy(buf + _len,
          "  -r, --record=FILE              "
          "Path to record video file. Using mkv extension is recommended.\n"
+         "      --hash=HASH                "
+         "Force a ROM hash. Not mandatory.\n"
          "      --recordconfig             "
          "Path to settings used during recording.\n"
          "      --size=WIDTHxHEIGHT        "
@@ -6989,6 +6992,7 @@ static bool retroarch_parse_input_and_config(
 #endif /* HAVE_XDELTA */
       { "no-patch",           0, NULL, RA_OPT_NO_PATCH },
 #endif /* HAVE_PATCH */
+      { "hash",               1, NULL, RA_OPT_HASH },
       { "detach",             0, NULL, 'D' },
       { "features",           0, NULL, RA_OPT_FEATURES },
       { "subsystem",          1, NULL, RA_OPT_SUBSYSTEM },
@@ -7505,6 +7509,11 @@ static bool retroarch_parse_input_and_config(
 
                configuration_set_string(settings,
                      settings->paths.username, optarg);
+               break;
+
+            case RA_OPT_HASH:
+               sscanf(optarg, "%8X", &runloop_st->name.hash);
+               RARCH_LOG("Got CRC32 from command line: 0x%x .\n", runloop_st->name.hash);
                break;
 
             case RA_OPT_SIZE:
